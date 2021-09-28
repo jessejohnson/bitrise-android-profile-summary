e4c6c6a028295b8a060e5d0360bd72805cf112e1
Jesse Johnson <akpene@Jesses-MacBook-Pro.local>
2021-09-28 20:47:39 +0000

    Remove spurious env var, fix location of python script

--- scripts/parse_profile_report.sh
+++ scripts/parse_profile_report.sh
@@ -2,4 +2,4 @@
 
 echo "Parsing Android build"
 pip install beautifulsoup4
-python3 $BITRISE_STEP_SOURCE_DIR/scripts/buildreport/parse_report.py ./build/reports/profile/ | envman add --key REPORT_SUMMARY
+python3 $BITRISE_STEP_SOURCE_DIR/scripts/parse_report.py ./build/reports/profile/ | envman add --key REPORT_SUMMARY
