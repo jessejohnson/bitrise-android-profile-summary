ac50ac64116c2fb1f4ba7748b8bd67bb77b21e1a
Jesse Johnson <akpene@Jesses-MacBook-Pro.local>
2021-09-28 23:36:13 +0000

    Call python 2.7

--- scripts/parse_profile_report.sh
+++ scripts/parse_profile_report.sh
@@ -2,5 +2,5 @@
 
 echo "Parsing Android build"
 # pip install beautifulsoup4
-apt-get install python-bs4
-python3 $BITRISE_STEP_SOURCE_DIR/scripts/parse_report.py ./build/reports/profile/ | envman add --key REPORT_SUMMARY
+apt-get install -y python-bs4
+python $BITRISE_STEP_SOURCE_DIR/scripts/parse_report.py ./build/reports/profile/ | envman add --key REPORT_SUMMARY
