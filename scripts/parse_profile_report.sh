f34312dba97bf7992a7aa07ff8c5976da33d5783
Jesse Johnson <akpene@Jesses-MacBook-Pro.local>
2021-09-28 23:29:32 +0000

    Use platform's BS4 in Python 2.7

--- scripts/parse_profile_report.sh
+++ scripts/parse_profile_report.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Parsing Android build"
-pip install beautifulsoup4
+# pip install beautifulsoup4
+apt-get install python-bs4
 python3 $BITRISE_STEP_SOURCE_DIR/scripts/parse_report.py ./build/reports/profile/ | envman add --key REPORT_SUMMARY
