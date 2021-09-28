c3fd0563a88771f636e208da47f144ad385354bc
Jesse Johnson <akpene@Jesses-MacBook-Pro.local>
2021-09-28 23:43:52 +0000

    Fix syntax

--- scripts/parse_report.py
+++ scripts/parse_report.py
@@ -13,18 +13,18 @@ def parse_report():
     first_table = soup.find_all('table')[0]
     summary_row = first_table.find_all('tr')[1]
     total_duration_cell = summary_row.find_all('td')[1]
-    print "*Total Build Time was {0}*".format(total_duration_cell.string)
+    print("*Total Build Time was {0}*".format(total_duration_cell.string))
 
     # then we print the time it took each module (ignoring subtasks) to build
     last_table = soup.find_all('table')[-1]
     all_rows = last_table.find_all('tr')
-    print "Breakdown\n---------"
-    print "Module       Duration"
+    print("Breakdown\n---------")
+    print("Module       Duration")
     for row in all_rows[1:-1]:
         first_cell = row.td
         if not first_cell.attrs:
             module_name = row.find_all('td')[0].string
             duration = row.find_all('td')[1].string
-            print "`{0}`      {1}".format(module_name, duration)
+            print("`{0}`      {1}".format(module_name, duration))
 
 parse_report()
