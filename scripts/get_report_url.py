effdf6b2e6d38569c50cff0244301b91870dcc8b
Jesse Johnson <akpene@Jesses-MacBook-Pro.local>
2021-09-29 23:19:03 +0000

    Add error handling for missing artifact

--- scripts/get_report_url.py
+++ scripts/get_report_url.py
@@ -20,6 +20,9 @@ def get_profile_artifact_slug():
                 return artifact['slug']
 
 def print_artifact_url(slug):
+    if slug is None:
+        print("We could not get the artifact slug. Did you `--profile` your Gradle task?")
+        return
     artifact_url = ARTIFACT_BASE_URL + "/" + slug
     response = requests.get(artifact_url, headers=HEADERS)
     if response.status_code == 200:
