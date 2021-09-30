0fdc0f5e051e597499d3e3e732e422cebb6d767c
Jesse Johnson <akpene@Jesses-MacBook-Pro.local>
2021-09-30 18:22:59 +0000

    Get permanent download url from env var with API call fallback

--- scripts/get_report_url.py
+++ scripts/get_report_url.py
@@ -3,6 +3,7 @@ import sys, os, requests
 BITRISE_API_ACCESS_TOKEN = sys.argv[1]
 APP_SLUG = sys.argv[2]
 BUILD_SLUG = sys.argv[3]
+ARTIFACT_URL_MAP = os.environ['BITRISE_PERMANENT_DOWNLOAD_URL_MAP']
 
 BASE_URL = "https://api.bitrise.io/v0.1"
 ARTIFACT_BASE_URL = BASE_URL + "/apps/" + APP_SLUG + "/builds/" + BUILD_SLUG + "/artifacts"
@@ -10,6 +11,14 @@ HEADERS = {
     "Authorization": BITRISE_API_ACCESS_TOKEN,
     "Content-Type": "application/json"
 }
+
+def get_permanent_download_url():
+    if ARTIFACT_URL_MAP is not None:
+        artifacts = ARTIFACT_URL_MAP.split("|")
+        for artifact in artifacts:
+            if "profile.zip" in artifact:
+                return artifact.split("=>")[1]
+    return None
 
 def get_profile_artifact_slug():
     response = requests.get(ARTIFACT_BASE_URL, headers=HEADERS)
@@ -19,14 +28,18 @@ def get_profile_artifact_slug():
             if artifact['title'] == 'profile.zip':
                 return artifact['slug']
 
-def print_artifact_url(slug):
+def get_artifact_url(slug):
     if slug is None:
         print("We could not get the artifact slug. Did you `--profile` your Gradle task?")
     artifact_url = ARTIFACT_BASE_URL + "/" + slug
     response = requests.get(artifact_url, headers=HEADERS)
     if response.status_code == 200:
         artifact_response = response.json()
-        print(artifact_response['data']['expiring_download_url'])
+        return artifact_response['data']['expiring_download_url']
 
-artifact_slug = get_profile_artifact_slug()
-print_artifact_url(artifact_slug)
+download_url = get_permanent_download_url()
+if download_url is None:
+    artifact_slug = get_profile_artifact_slug()
+    download_url = get_artifact_url(artifact_slug)
+
+print(download_url)
