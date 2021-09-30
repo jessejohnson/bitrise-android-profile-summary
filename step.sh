6c0d87554f91d08e7d9e2e8bd17a5b5935af05b5
Jesse Johnson <akpene@Jesses-MacBook-Pro.local>
2021-09-30 01:26:07 +0000

    Fix report path error

--- step.sh
+++ step.sh
@@ -3,6 +3,6 @@ set -ex
 
 pip3 install beautifulsoup4
 
-python3 $BITRISE_STEP_SOURCE_DIR/scripts/parse_report.py ./build/reports/profile/d/ | envman add --key REPORT_SUMMARY
+python3 $BITRISE_STEP_SOURCE_DIR/scripts/parse_report.py ./build/reports/profile/ | envman add --key REPORT_SUMMARY
 python3 $BITRISE_STEP_SOURCE_DIR/scripts/get_report_url.py \
 $BITRISE_API_ACCESS_TOKEN $BITRISE_APP_SLUG $BITRISE_BUILD_SLUG | envman add --key ARTIFACT_URL
